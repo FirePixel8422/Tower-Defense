04753dbd2c487b611ec77b54a83a976c3c1add00
FirePixel8422 <123118726+FirePixel8422@users.noreply.github.com>
2024-05-03 19:47:57 +0200

    Sync
    what a mess below :(
    learned what revert does, laptop to pc sync

--- Assets/C# Scripts/Towers/TowerCore.cs
+++ Assets/C# Scripts/Towers/TowerCore.cs
@@ -1,32 +1,42 @@
+using JetBrains.Annotations;
 using System.Collections;
 using UnityEngine;
 
 public class TowerCore : MonoBehaviour
 {
+    #region disolveEffect
     public Material dissolveMaterial;
 
     private float cDissolveEffectState;
     public float startDissolveEffectState;
     public float dissolveSpeed;
     public float endDisolveValue;
+    #endregion
 
-    public SpriteRenderer towerPreviewRenderer;
+
+    public SpriteRenderer towerRange;
+
+
+    public Element[] elements;
+    public float attackSpeed;
+    public float range;
 
 
 
     private void Start()
     {
         dissolveMaterial = GetComponent<Renderer>().material;
-        towerPreviewRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        towerRange = GetComponentInChildren<SpriteRenderer>();
+        towerRange.transform.localScale = Vector3.one * range;
     }
     public void Init()
     {
         StartCoroutine(CreateTower());
     }
-
     private IEnumerator CreateTower()
     {
-        towerPreviewRenderer.enabled = false;
+        towerRange.enabled = false;
 
         cDissolveEffectState = startDissolveEffectState;
         while (cDissolveEffectState > endDisolveValue)
