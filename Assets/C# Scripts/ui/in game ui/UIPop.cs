60b4b3aa951a362353e071436dd974337fd7a12d
Despair0090 <benniemeinen4@gmail.com>
2024-05-29 12:35:18 +0200

    ui changes

--- Assets/C# Scripts/ui/in game ui/UIPop.cs
+++ Assets/C# Scripts/ui/in game ui/UIPop.cs
@@ -6,32 +6,21 @@ using UnityEngine.EventSystems;
 public class UIPop : MonoBehaviour, IPointerEnterHandler , IPointerExitHandler
 {
     public GameObject PopUpText;
+    public GameObject PopDownText;
 
     public void Start()
     {
         PopUpText.SetActive(false);
     }
-
-    public void OnMouseOver()
-    {
-        PopUpText.SetActive(true);
-        print("over");
-    }
-
-    public void OnMouseExit()
-    {
-        PopUpText.SetActive(false);
-        print("exit");
-
-    }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
         PopUpText.SetActive(true);
+        PopDownText.SetActive(false);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         PopUpText.SetActive(false);
+        PopDownText.SetActive(true);
     }
 }
