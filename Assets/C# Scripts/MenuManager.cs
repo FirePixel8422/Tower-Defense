4ae497cd41bc1d4cfece53b06ce748eefd5929d1
FirePixel8422 <bmtminecraft@hotmail.com>
2024-06-14 13:45:36 +0200

    Revert "Balance attampt added ui"
    This reverts commit dca1d79efdd519d96dd5e366c4eff43eb8437a9e.

--- Assets/C# Scripts/MenuManager.cs
+++ Assets/C# Scripts/MenuManager.cs
@@ -6,13 +6,12 @@ using UnityEngine.InputSystem;
 public class MenuManager : MonoBehaviour
 {
     public GameObject pauseMenuObj;
-    public GameObject[] settingsMenus;
 
     public bool paused;
 
     public void OnPause(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && SelectionManager.Instance.isPlacingTower == false && Input.GetKeyDown(KeyCode.Escape))
+        if (ctx.performed)
         {
             PauseRestartGame();
         }
@@ -25,29 +24,5 @@ public class MenuManager : MonoBehaviour
 
         Time.timeScale = paused ? 0 : 1;
         pauseMenuObj.SetActive(paused);
-
-        if (paused == false)
-        {
-            foreach (GameObject g in settingsMenus)
-            {
-                g.SetActive(false);
-            }
-        }
-    }
-    public void RestartGame()
-    {
-        paused = false;
-
-        Time.timeScale = 1;
-        pauseMenuObj.SetActive(false);
-        foreach(GameObject g in settingsMenus)
-        {
-            g.SetActive(false);
-        }
-    }
-
-    public void LoadMainMenu()
-    {
-
     }
 }
--- Assets/C# Scripts/WaveSystem/WaveManager.cs
+++ Assets/C# Scripts/WaveSystem/WaveManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using System.Linq;
 using UnityEngine;
-using UnityEditor;
 
 public class WaveManager : MonoBehaviour
 {
@@ -14,7 +13,6 @@ public class WaveManager : MonoBehaviour
     }
 
     public int waveScaleHealthDelay;
-    public int waveId;
     public float healthScaleMultiplier;
     public float cHealthScale = 1;
 
@@ -46,16 +44,10 @@ public class WaveManager : MonoBehaviour
     {
         yield return new WaitForSeconds(preparationTime);
 
-        bool startWaveDone = false;
         while (true)
         {
             for (int i = 0; i < waves.Length; i++)
             {
-                if (startWaveDone == true && i == 0)
-                {
-                    continue;
-                }
-
                 for (int i2 = 0; i2 < waves[i].waveParts.Length; i2++)
                 {
                     yield return new WaitForSeconds(waves[i].waveParts[i2].startDelay);
@@ -67,20 +59,15 @@ public class WaveManager : MonoBehaviour
                         spawnedObj.Add(target);
                         UpdateTargetDir(out target.dir, target.transform.position, startPointPos);
 
+                        ResourceManager.Instance.AddScrap(waves[i].waveParts[i2].scrapForThisWavePart);
 
                         yield return new WaitForSeconds(waves[i].waveParts[i2].spawnDelay);
                     }
-                    ResourceManager.Instance.AddScrap(waves[i].waveParts[i2].scrapForThisWavePart);
                 }
                 yield return new WaitForSeconds(waves[i].waveEndDelay);
-                waveId += 1;
-                if (waveId > waveScaleHealthDelay)
-                {
-                    cHealthScale += healthScaleMultiplier - 1;
-                }
+                cHealthScale += healthScaleMultiplier - 1;
                 ResourceManager.Instance.AddScrap(waves[i].scrapForThisWave);
             }
-            startWaveDone = true;
         }
     }
 
